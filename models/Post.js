af42e41f5b9c997a0143adaaf6b3b85b096a3091
Grant Kirill <webdotg.inbox@gmail.com>
2023-11-21 21:57:53 +0300

    add create post post

---
+++ models/Post.js
@@ -0,0 +1,34 @@
+import mongoose from 'mongoose'
+
+//необходтио создать схему нашей таблицы
+const PostSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true                          //required флаг обязательного поля для заполнения
+  },
+  text: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  tegs: {
+    type: Array,
+    default: []                              //если теги не указаны то создаётся пустой массив
+  },
+  viewsCount: {                             //счетчик просмотра статей
+    type: Number,
+    default: 0
+  },
+  user: {
+    type: mongoose.Schema.Types.ObjectId,   //автор статьи в BD записан в mongoose.Schema.Types.ObjectId 
+    ref: 'User',                     //ссылается на МОДЕЛЬ ЮЗЕР relationsheep/связь между жвумя таблицами
+    required: true
+  },
+  imageUrl: String,                        //required отстутсвует значит avatar можеь и не быть
+},
+  {
+    timestamps: true             //говорит что обязательно создание даты при создании и изменении статьи                  
+  },
+)
+
+export default mongoose.model('Post', PostSchema)
